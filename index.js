cc9d7295e8b0bd532f91c3cc861474f9505dd503
Coder Hillary <coderhillary@192.168.1.215>
2025-06-15 18:53:15 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function receivesAFunction(callback) {
+  callback();
+}
+
+
+function returnsANamedFunction() {
+  
+  function namedFunction() {
+    console.log("I'm a named function");
+  }
+  return namedFunction;
+}
+
+
+function returnsAnAnonymousFunction() {
+  return function() {
+    console.log("I'm an anonymous function");
+  };
+}
+
+
+module.exports = {
+  receivesAFunction,
+  returnsANamedFunction,
+  returnsAnAnonymousFunction,
+};
